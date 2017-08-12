031812c5e9baf131319c5d5b655653c57e0ab44a
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-08-12 13:35:34 +0300

    Bulls and Cows 2:Introducing the game

--- Bulls and Cows/ConsoleApplication1/Main.cpp
+++ Bulls and Cows/ConsoleApplication1/Main.cpp
@@ -1,1 +1,28 @@
-int main() { return 0; }
+#include <iostream>
+#include <string>
+
+// Declare the functions
+void IntroduceGame();
+void GetGuessAndPrintItBack();
+
+// To make it like unreal engine code
+using FString  = std::string;
+using int32 = int;
+
+int main() 
+{ 
+	IntroduceGame();
+	return 0;
+}
+
+void IntroduceGame() 
+{
+	constexpr int32 WORD_LENGTH = 5;
+	std::cout << "Welcome to bulls and cows a fun word game.\n";
+	std::cout << "Can you guess the "<< WORD_LENGTH<<" letter isogram I'm thinking of\n\n";
+}
+
+void GetGuessAndPrintItBack() // Has a side-effect
+{
+
+}
