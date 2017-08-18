d6aad050eb9cc7a0e698ea685c7ad69738c9f344
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-08-18 12:53:07 +0300

    Bulls and Cows 7:Including Header files

---
+++ Bulls and Cows/ConsoleApplication1/FBullCowGame.cpp
@@ -0,0 +1,28 @@
+#include "FBullCowGame.h"
+
+using int32 = int;
+
+void FBullCowGame::Reset() const
+{
+
+}
+
+int32 FBullCowGame::GetMaxTries()
+{
+	return 0;
+}
+
+int32 FBullCowGame::GetCurrentTry()
+{
+	return 0;
+}
+
+bool FBullCowGame::CheckGuessValidity()
+{
+	return false;
+}
+
+bool FBullCowGame::IsGameWon() const
+{
+	return false;
+}
